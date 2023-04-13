e73b4dfe806f9762738d298c570e163b1d5dcd32
SSlime-s <62363188+SSlime-s@users.noreply.github.com>
2023-04-14 00:03:48 +0900

    🎨 glassmorphism 用の class 追加

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,19 @@
+const GlassmorphismPlugin = ({ addComponents }) => {
+	addComponents({
+		'.glassmorphism': {
+			background: 'radial-gradient(ellipse at 0% 0%, rgba(255, 255, 255, 0.5) 0%, rgba(255, 255, 255, 0.4) 100%)',
+			border: '1px solid rgba(255, 255, 255, 0.5)',
+			boxShadow: '0 0 16px rgba(0, 0, 0, 0.25)',
+			backdropFilter: 'blur(12px)',
+		},
+	})
+}
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
 		extend: {},
 	},
-	plugins: [],
+	plugins: [GlassmorphismPlugin],
 }
